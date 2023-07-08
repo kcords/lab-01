bcd6d2cbcc5381949f7e34a334486803fa397e35
Kyle Cordell <kcordell-git@outlook.com>
2023-07-08 14:17:16 -0700

    Adds Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Header extends React.Component{
+
+  render() {
+    return(
+      <header>
+        <h1>This is an application. Yay!</h1>
+      </header>
+    )
+  }
+}
+
+export default Header;
