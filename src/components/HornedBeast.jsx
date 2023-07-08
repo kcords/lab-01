79684181da61167d4ec9818611f09688ddd6d1d8
Kyle Cordell <kcordell-git@outlook.com>
2023-07-08 14:20:04 -0700

    Adds HornedBeast component

---
+++ src/components/HornedBeast.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class HornedBeast extends React.Component{
+  render(){
+    return(
+      <>
+        <h2>Animal Title</h2>
+        <img />
+        <p>Description</p>
+      </>
+    )
+  }
+}
+
+export default HornedBeast;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import HornedBeast from './components/HornedBeast'
 
 class Main extends React.Component{
   render() {
     return(
       <main>
+        <HornedBeast title={''} imageUrl={''} description={''} />
       </main>
     )
   }
