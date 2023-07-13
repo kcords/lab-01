43c18858c9f34c65641a54e67dd0d16df92169be
Kyle Cordell <kcordell-git@outlook.com>
2023-07-12 19:55:36 -0700

    Adds responsive grid layout using bootstrap container, row and cols

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col';
+
 import HornedBeast from './HornedBeast'
 
 class Main extends React.Component{
@@ -6,12 +11,19 @@ class Main extends React.Component{
     const { beastData } = this.props;
     return(
       <main>
-        {beastData.map(beast => <HornedBeast
-          key={beast._id}
-          title={beast.title}
-          image={beast.image_url}
-          description={beast.description}
-        />)}
+        <Container fluid>
+          <Row>
+            {beastData.map(beast =>
+             <Col key={beast._id} xs={12} sm={6} md={4} lg={3} xl={2}>
+              <HornedBeast
+                title={beast.title}
+                image={beast.image_url}
+                description={beast.description}
+              />
+             </Col>
+            )}
+          </Row>
+        </Container>
       </main>
     )
   }
