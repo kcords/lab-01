bfafad9f4d2b4699546921bd191d42b4c3406708
Kyle Cordell <kcordell-git@outlook.com>
2023-07-12 18:56:33 -0700

    Updates Main and HornedBeast component props to align with requirements

--- src/components/HornedBeast.jsx
+++ src/components/HornedBeast.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 
 class HornedBeast extends React.Component{
   render(){
+    const {title, image, description} = this.props;
     return(
       <>
-        <h2>{this.props.title}</h2>
-        <img src={this.props.imageUrl} alt={this.props.description} />
-        <p>{this.props.description}</p>
+        <h2>{title}</h2>
+        <img
+          src={image}
+          alt={description}
+          style={{maxWidth: '200px'}}
+        />
+        <p>{description}</p>
       </>
     )
   }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,13 +3,14 @@ import HornedBeast from './HornedBeast'
 
 class Main extends React.Component{
   render() {
+    const { beastData } = this.props;
     return(
       <main>
-        {this.props.animalData.map(animal => <HornedBeast
-          key={animal._id}
-          title={animal.title}
-          imageUrl={animal.image_url}
-          description={animal.description}
+        {beastData.map(beast => <HornedBeast
+          key={beast._id}
+          title={beast.title}
+          image={beast.image_url}
+          description={beast.description}
         />)}
       </main>
     )
