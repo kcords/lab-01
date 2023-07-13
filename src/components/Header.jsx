3963f6e13d4174eac216fb8a873b57ef79d01309
Kyle Cordell <kcordell-git@outlook.com>
2023-07-12 21:19:07 -0700

    Adds a fun header interaction

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 
 class Header extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: "Horned Animals!",
+      color: "primary"
+    }
+  }
+
+  handleClick = () => {
+    this.setState({
+      title: "Whimsical Pokey Beings... Ouch!",
+      color: "danger"
+    })
+  }
 
   render() {
     return(
-      <header>
-        <h1>Horned Animals!</h1>
+      <header
+        className={`p-md-4 d-inline-block text-${this.state.color}`}
+        role="button"
+      >
+        <h1 onClick={this.handleClick}>{this.state.title}</h1>
       </header>
     )
   }
