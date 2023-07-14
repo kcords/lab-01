8efa11ea1723402ed4314caca13059f98dffdf14
Kyle Cordell <kcordell-git@outlook.com>
2023-07-13 20:45:24 -0700

    Adds stretch feature color mode theme toggle

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 
 export default class Footer extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      mode: 'light'
+    }
+  }
+
+  toggleMode = () => {
+    this.setState(({mode}) => {
+      const newMode = mode === 'light' ? 'dark' : 'light';
+      const html = document.querySelector('html');
+      html.setAttribute('data-bs-theme', newMode)
+      return {mode: newMode}
+    })
+  }
+
   render() {
     return(
-      <footer>
+      <footer role="button" onClick={this.toggleMode}>
         <p>&copy; 2021 Kyle Cordell</p>
       </footer>
     )
