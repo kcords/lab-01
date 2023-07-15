93cee8dc8f89fd06634bfb291b0244aa49db2327
Kyle Cordell <kcordell-git@outlook.com>
2023-07-15 15:29:03 -0700

    Adds description to the modal to match feature requirements

--- src/components/SelectedBeast.jsx
+++ src/components/SelectedBeast.jsx
@@ -6,14 +6,15 @@ import Button from 'react-bootstrap/Button';
 export default class SelectedBeast extends React.Component {
   render() {
     const { showModal, selectedBeast, handleBeastSelection } = this.props;
-    const { title, image_url } = selectedBeast || {};
+    const { title, image_url, description } = selectedBeast || {};
     return(
       <Modal show={showModal} onHide={() => { handleBeastSelection(null) }}>
         <Modal.Header closeButton>
           <Modal.Title>{title}</Modal.Title>
         </Modal.Header>
-        <Modal.Body>
-          <Image src={image_url} fluid/>
+        <Modal.Body className="text-center">
+          <Image src={image_url} fluid className="mb-3" />
+          <p>{ description }</p>
         </Modal.Body>
         <Modal.Footer>
           <Button
