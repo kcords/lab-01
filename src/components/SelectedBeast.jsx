1046c29e323a946e07c1061867efd427b74c4f29
Kyle Cordell <kcordell-git@outlook.com>
2023-07-15 12:12:13 -0700

    Adds SelectedBeast component with Modal

--- src/components/SelectedBeast.jsx
+++ src/components/SelectedBeast.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Image from 'react-bootstrap/Image'
+import Button from 'react-bootstrap/Button';
 
 export default class SelectedBeast extends React.Component {
   render() {
+    const { showModal, selectedBeast, handleBeastSelection } = this.props;
+    const { title, image_url } = selectedBeast || {};
     return(
-      <>
-      </>
+      <Modal show={showModal} onHide={() => { handleBeastSelection(null) }}>
+        <Modal.Header closeButton>
+          <Modal.Title>{title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Image src={image_url} fluid/>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="secondary"
+            onClick={() => { handleBeastSelection(null) }}
+          >
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     );
   }
 }
