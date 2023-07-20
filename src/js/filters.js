48a1b658e83b8cc76b3b981985171e9182588511
Kyle Cordell <kcordell-git@outlook.com>
2023-07-19 20:52:03 -0700

    Moves filter functionality to external file

---
+++ src/js/filters.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const filterBeasts = (beastList, filters) => {
+  const { searchTerm, horns } = filters || {};
+  console.log({ beastList, filters });
+  return beastList.filter((beast) => {
+    if (!searchTerm.length && horns === "All") return true;
+    const fuzzySearchPattern = searchTerm
+      .split(" ")
+      .map((term) => {
+        return term
+          .replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&")
+          .replace(/./g, "(?=.*$&)");
+      })
+      .join("");
+    const regex = new RegExp(fuzzySearchPattern, "i");
+    const isSearchTermMatch =
+      !searchTerm.length ||
+      regex.test(beast.title) ||
+      regex.test(beast.description);
+    const isHornsMatch = horns === "All" || beast.horns === parseInt(horns);
+
+    return isSearchTermMatch && isHornsMatch;
+  });
+};
+
+export default filterBeasts;
