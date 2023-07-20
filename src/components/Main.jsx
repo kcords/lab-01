190f150a060e445b65f0a5b5ee161e2de0edd461
Kyle Cordell <kcordell-git@outlook.com>
2023-07-19 20:16:22 -0700

    Adds Form.Select for horns filter

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,94 +3,80 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col';
-import InputGroup from 'react-bootstrap/InputGroup'
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import Button from "react-bootstrap/Button";
 
-import HornedBeast from './HornedBeast'
+import HornedBeast from "./HornedBeast";
 
-export default class Main extends React.Component{
+export default class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchTerm: ''
-    }
-  }
-
-  handleUpdateSearch = ({target: { value: searchTerm }}) => {
-    this.setState({ searchTerm })
+      filteredBeasts: this.props.beastData,
+      uniqueHornCounts: [
+        "All",
+        ...Array.from(
+          new Set(this.props.beastData.map(({ horns }) => horns))
+        ).sort((a, b) => a - b),
+      ],
+    };
   }
 
   render() {
-    const { beastData, handleBeastSelection } = this.props;
-    return(
+    const { handleBeastSelection } = this.props;
+    return (
       <main>
         <Container fluid>
           <Row className="sticky-top">
-            <Col
-              xs={12}
-              sm={12}
-              md={8}
-              lg={6}
-              xl={4}
-            >
-              <InputGroup className="mb-3">
-                <InputGroup.Text id="search">
-                  Search
-                </InputGroup.Text>
-                <Form.Control
-                  aria-label="Search"
-                  aria-describedby="search"
-                  value={this.state.searchTerm}
-                  onInput={this.handleUpdateSearch}
-                />
-              <Button
-                variant="outline-secondary"
-                id="clear-search"
-                onClick={() => {
-                  this.setState({searchTerm: ''})
-                }}
-              >
-                &#10005;
-              </Button>
-              </InputGroup>
+            <Col xs={12} sm={12} md={10} lg={10} xl={8}>
+              <Form>
+                <InputGroup className="mb-3">
+                  <InputGroup.Text id="search">Search</InputGroup.Text>
+                  <Form.Control
+                    aria-label="Search"
+                    aria-describedby="search"
+                    name="searchTerm"
+                  />
+                  <InputGroup.Text id="horn-filter">
+                    Horn Filter
+                  </InputGroup.Text>
+                  <Form.Select name="horns">
+                    {this.state.uniqueHornCounts.map((label) => (
+                      <option key={label}>{label}</option>
+                    ))}
+                  </Form.Select>
+                  <Button
+                    variant="outline-secondary"
+                    id="clear-search"
+                    type="reset"
+                  >
+                    &#10005;
+                  </Button>
+                </InputGroup>
+              </Form>
             </Col>
           </Row>
           <Row className="mb-4">
-            {beastData
-              .filter(beast => {
-                if (!this.state.searchTerm) return true;
-                const fuzzySearchPattern = this.state.searchTerm
-                  .split(' ')
-                  .map(term => {
-                    return term
-                      .replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&")
-                      .replace(/./g, "(?=.*$&)");
-                  })
-                  .join('');
-                const regex = new RegExp(fuzzySearchPattern, "i");
-                return regex.test(beast.title) || regex.test(beast.description)
-              })
-              .map(beast =>
-                <Col
-                  key={beast._id}
-                  xs={12}
-                  sm={6}
-                  md={4}
-                  lg={3}
-                  xl={2}
-                  className="d-flex"
-                >
-                  <HornedBeast
-                    beast={beast}
-                    handleBeastSelection={handleBeastSelection}
-                  />
-                </Col>
-              )
-            }
+            {this.state.filteredBeasts.map((beast) => (
+              <Col
+                key={beast._id}
+                xs={12}
+                sm={6}
+                md={4}
+                lg={3}
+                xl={2}
+                className="d-flex"
+              >
+                <HornedBeast
+                  beast={beast}
+                  handleBeastSelection={handleBeastSelection}
+                />
+              </Col>
+            ))}
           </Row>
         </Container>
       </main>
-    )
+    );
   }
 }
